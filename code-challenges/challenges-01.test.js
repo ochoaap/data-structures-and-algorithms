0eed1af305f0404d8b8dcaceeaca602d4f3a414f
Anthony Ochoa <ochoaap@yahoo.com>
2020-12-08 03:36:12 -0800

    working on problems

--- code-challenges/challenges-01.test.js
+++ code-challenges/challenges-01.test.js
@@ -67,6 +67,9 @@ This function should use forEach to populate your grocery list based on the stor
 
 const createList = (availableItems) => {
   // Solution code here...
+  createList.forEach(let i = 0; i < availableItems; i++ => {
+    
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
